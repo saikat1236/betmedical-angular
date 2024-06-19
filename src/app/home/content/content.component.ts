eb03c3feae260703c94848b3b960330cb1dad5bd
Saikat Biswas <saikat1236@gmail.com>
2024-06-20 00:50:46 +0530

    responsiveness and styling 3

--- src/app/home/content/content.component.ts
+++ src/app/home/content/content.component.ts
@@ -5,6 +5,74 @@ import { Component } from '@angular/core';
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.scss']
 })
+
 export class Content {
-  
+  showAll = false;
+
+  visibleItems = [
+    {
+      imgSrc: 'https://cdn.builder.io/api/v1/image/assets/TEMP/7786565e71b1f7d283e84ee17661e4b9b90a11aa3c4af772368c20693f0e099f?apiKey=e4359e71dc7544f59d6c1a8f2e8a7240&',
+      imgAlt: 'Medical Portfolio 1',
+      title: 'Mobile Imaging System',
+      description: '2D & 3D Imaging Solutions from Ziehm Imaging, Germany; 50 years of continuous innovation'
+    },
+    {
+      imgSrc: 'https://cdn.builder.io/api/v1/image/assets/TEMP/a858fcf475dbdc1428fc3934ecd479b37502c63905f599eee80b9f06023cc80c?apiKey=e4359e71dc7544f59d6c1a8f2e8a7240&',
+      imgAlt: 'Medical Portfolio 2',
+      title: 'Spine Tables',
+      description: 'Smooth and Flexible Carbon Fibre Spine attachment for surgical Table from ISO Medical Systems Inc, Japan'
+    },
+    {
+      imgSrc: 'https://cdn.builder.io/api/v1/image/assets/TEMP/e977e2199634e6789b420a7ac5ec413244a9b8bb5af3e246b4db47f7b202698e?apiKey=e4359e71dc7544f59d6c1a8f2e8a7240&',
+      imgAlt: 'Medical Portfolio 4',
+      title: 'Surgical LED or Lights',
+      description: 'High Efficiency, Superior Optical Performance Surgical LED OR Lights From BenQ Medical Technology Corporation, Taiwan'
+    },
+    {
+      imgSrc: '../../../assets/Rectangle 5317.png',
+      imgAlt: 'Medical Portfolio 3',
+      title: 'Surgical Or Tables',
+      description: 'High Quality, Reliable, Radiolucent and Innovative Surgical OR Tables From Benq Medical Technology'
+    },
+    {
+      imgSrc: '../../../assets/Rectangle 5333.png',
+      imgAlt: 'Medical Portfolio 5',
+      title: 'Mini C-Arm',
+      description: 'State-Of-The-Art Innovative Solutions For Orthopedic Imaging from OrthoscanInc, USA; Global Leader In Mini C-Arm Imaging'
+    },
+    {
+      imgSrc: '../../../assets/Rectangle 5333 2.png',
+      imgAlt: 'Medical Portfolio 6',
+      title: 'Imaging Tables',
+      description: 'Full Carbon Fibre Table Compatible with 3d & 2d C-Arm for Vascular catheterization and Orthopedic Navigation surgeries from Hotborn Medical Equipment Co., Ltd., China'
+    },
+    {
+      imgSrc: '../../../assets/Rectangle 5317 2.png',
+      imgAlt: 'Medical Portfolio 3',
+      title: 'Criticalcare Ventilators',
+      description: 'A state-of-the-art piston-type HFO ventilator that brings together over 30 years’ worth of technological achievements MetranCo.,Ltd, Japan'
+    },
+    {
+      imgSrc: '../../../assets/Rectangle 5333 (2).png',
+      imgAlt: 'Medical Portfolio 5',
+      title: 'Breast Imaging',
+      description: 'State of the art solutions for screening and breast tomosynthesis, High-performance mammography systems with proven reliability from Metaltronica, Italy.'
+    },
+    {
+      imgSrc: '../../../assets/Rectangle 5333 (3).png',
+      imgAlt: 'Medical Portfolio 6',
+      title: 'Digital Radiography',
+      description: 'Standard and Mobile digital X-ray system with high durability and Reliability from SITEC Medical Co.,Ltd., Korea'
+    },
+    {
+      imgSrc: '../../../assets/Rectangle 5333 2.png',
+      imgAlt: 'Medical Portfolio 6',
+      title: 'Cardiovascular Fusion imaging & Navigation',
+      description: 'Advanced And User-Friendly AI Imaging Software for Endovascular Case Planning To Intra-Operative Navigation from Therenva, France'
+    }
+  ];
+
+  toggleShow() {
+    this.showAll = !this.showAll;
+  }
 }
